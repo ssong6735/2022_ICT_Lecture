4863188b03cf9c5e77355cbac8b5d2d5d7e6d919
ssong6735 <because6735@gmail.com>
2022-05-16 15:13:25 +0900

    [추가] : java_lecture 프로젝트 커밋

---
+++ java_lecture/src/java_lecture/main.java
@@ -0,0 +1,10 @@
+package java_lecture;
+
+public class main {
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.println(num);
+		num = 20;
+		System.out.println(num);		
+	}
+}
