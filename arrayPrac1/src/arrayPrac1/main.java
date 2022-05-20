ec402c2b496fe6f2e5ba391180ae8ce7a3c62014
ssong6735 <because6735@gmail.com>
2022-05-20 16:56:18 +0900

    [추가] : arrayPrac1 프로젝트 커밋

---
+++ arrayPrac1/src/arrayPrac1/main.java
@@ -0,0 +1,39 @@
+package arrayPrac1;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		// 1. 다음의 출력 결과를 가지는 2차원 배열 코드를 작성하시오
+		// (size: 9x9, variable: int)
+		System.out.println("2차원 배열 출력 프로그램 시작!");
+		int[][] arr = new int[9][9];
+		for(int i=0;i<9;i++) { // y축 반복
+			for(int j=0;j<9;j++) { // x축 반복	
+				arr[i][j] = (i+1)*(j+1); 
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+
+		System.out.println();
+		
+		// 2. 다음의 출력 결과를 가지는 2차원 배열 코드를 작성하시오
+		// (size: 3x9, variable: char)
+		System.out.println("2차원 배열 출력 프로그램 시작!");
+		char[][] arr2 = new char[3][9];
+		char var = 'a';
+		for(int i=0;i<3;i++) { // y축 반복
+			for(int j=0;j<9;j++) { // x축 반복
+				int varChar = (int)var++;
+				if(varChar==123) {
+					varChar = 45;
+				}
+				arr2[i][j] = (char)varChar; 
+				System.out.print(arr2[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+}
