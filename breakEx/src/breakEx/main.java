3634985bf0d3f92b55fab59f75336de304d35103
ssong6735 <because6735@gmail.com>
2022-05-24 16:34:26 +0900

    [추가] : breakEx 프로젝트 커밋

---
+++ breakEx/src/breakEx/main.java
@@ -0,0 +1,16 @@
+package breakEx;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<9;i++) {
+			for(int j=0;j<9;j++) {
+				System.out.print("("+i+","+j+")");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+}
