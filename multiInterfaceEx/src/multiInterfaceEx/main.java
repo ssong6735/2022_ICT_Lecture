b91bd307dfaa1fa97e665ce508715060e9731bb2
ssong6735 <because6735@gmail.com>
2022-05-26 16:44:42 +0900

    [추가] : multiInterfaceEx 프로젝트 커밋

---
+++ multiInterfaceEx/src/multiInterfaceEx/main.java
@@ -0,0 +1,20 @@
+package multiInterfaceEx;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Customer customer = new Customer();
+		Buy buyer = customer;
+		buyer.buy();
+		
+		Sell seller = customer;
+		seller.sell();
+		
+		customer.order();
+		buyer.order();
+		seller.order();
+		
+	}
+	
+}
